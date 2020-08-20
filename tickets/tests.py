47d0ceb153018d68ef59ce00a7e13eb869d51d23
phaniKumar <chinnaphani@gmail.com>
2020-08-21 01:43:00 +0530

    testupdate

--- tickets/tests.py
+++ tickets/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
 
+
+import win32com.client
+from datetime import datetime
+
+
+strComputer = ["test","DESKTOP-BA9JKGE","testkind"]
+services = ['BthAvctpSvc','BFE','bits']
+objWMIService = win32com.client.Dispatch("WbemScripting.SWbemLocator")
+
+for srv in strComputer:
+    try:
+        squery = 0
+        objSWbemServices = objWMIService.ConnectServer(srv, "root\cimv2")
+        ucolitem = objSWbemServices.ExecQuery("Select * from Win32_OperatingSystem")
+        dcolitem = objSWbemServices.ExecQuery("Select * from Win32_LogicalDisk WHERE DriveType=3")
+        for uitem in ucolitem:
+            stime = uitem.LastBootUpTime
+            tests = stime.split('.')
+            datetimeformat = '%Y%m%d%H%M%S'
+            sftime = datetime.strptime(tests[0], datetimeformat)
+            timenow = datetime.now()
+            diff = timenow-sftime
+            diffs = diff.days
+            print(srv + ' is up for days ' + str(diffs))
+
+        for ditem in dcolitem:
+            size = ditem.Freespace
+            formatsize = int(size)/1024/1024/1024
+            print(srv + ditem.Name, int(round(formatsize)))
+
+        while squery<len(services):
+            test = services[squery]
+            testquery = "Select * from Win32_Service Where Name = '{}'".format(test)
+            colItems = objSWbemServices.ExecQuery(testquery)
+            for objitem in colItems:
+                try:
+                    print(srv +' '+ objitem.DisplayName, objitem.State, objitem.Name)
+                except:
+                    print("service not found")
+            squery = squery + 1
+    except:
+        print (srv + " not reachable")
+
 # Create your tests here.
